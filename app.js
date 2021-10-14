77d8075dcad6b9eaa19bbb6043e472d9e070ddc8
Lesly <leslyr562@gmail.com>
2021-10-14 17:06:31 -0500

    html updated

--- app.js
+++ app.js
@@ -129,16 +129,16 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
 
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 
 
-
-// const fs = require('fs');
-// const generatePage = require('.src/page-template.js');
-
-// const pageHTML= generatePage(name,github);
 
 
 
